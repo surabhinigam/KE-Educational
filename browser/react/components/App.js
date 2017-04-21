95420e299d05b26f2fe2749cf5b435a3d43efcbe
Surabhi Nigam <surabhi.nigam1@gmail.com>
2017-04-21 13:25:13 -0400

    making responsive by adding bootstrap

--- browser/react/components/App.js
+++ browser/react/components/App.js
@@ -31,10 +31,7 @@ export default class KEHome extends React.Component {
     WHAT KID EXPERTS DO 
   </p>
   <div className="react-row row">
-  <div className="col-2">
-    
-  </div>
-  <div className="col-2">
+  <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4">
   
     <figure>
       <img className="react-icons" src="baby.png" alt="Baby" height="130" width="130" />
@@ -42,20 +39,15 @@ export default class KEHome extends React.Component {
 (meals, naps, pick up)</p></figcaption>
     </figure>
   </div>
-  <div className="col-1">
-    
-  </div>
-  <div className="col-2">
+  <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4" >
   <figure>
       <img className="react-icons" src="drum.png" alt="Ideas" height="130" width="130" />
       <figcaption><span className="react-purple-text">ACTIVITIES </span><p className="react-description">Incorporate unique, enriching
    activities into childcare</p></figcaption>
     </figure> 
   </div>
-  <div className="col-1">
-    
-  </div>
-  <div className="col-2">
+  
+  <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4" >
     <figure>
       <img className="react-icons" src="gift.png" alt="Materials" height="130" width="130" />
       <figcaption><span className="react-purple-text">SUPPLIES</span><p className="react-description">Bring materials and props
@@ -63,43 +55,28 @@ to every session</p></figcaption>
     </figure>
     
   </div>
-  <div className="col-2">
-    
-  </div>
   </div>
   <section className="react-who-ke-are">
   <p className="react-heading">WHO KID EXPERTS ARE</p>
-    <div className="react-row">
-      <div className="col-2">
-        
-      </div>
-      <div className="col-2">
+    <div className="react-row row">
+      <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4" >
         <figure>
           <img className="react-icons" src="compass-transparent.png" alt="Knowledge" height="130" width="130"/>
           <figcaption><span className="react-purple-text">BRILLIANT </span><p className="react-description">Artists, teachers and teaching assistants with professional teaching experience</p></figcaption>
         </figure>
       </div>
-      <div className="col-1">
-        
-      </div>
-      <div className="col-2">
+      <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4" >
       <figure>
           <img className="react-icons" src="diaper2.png" alt="Alphabets" height="130" width="130"/>
           <figcaption><span className="react-purple-text">EXPERIENCED</span><p className="react-description">Baby whispers and kid gurus with childcare/babysitting experience</p></figcaption>
           
         </figure> 
       </div>
-      <div className="col-1">
-        
-      </div>
-      <div className="col-2">
+      <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4" >
         <figure>
           <img className="react-icons" src="light.png" alt="Light" height="130" width="130"/>
           <figcaption><span className="react-purple-text">INSPIRING</span><p className="react-description">Inspiring role models and good vibes people who love kids</p></figcaption>
         </figure>
-        
-      </div>
-      <div className="col-2">
         
       </div>
     </div>
